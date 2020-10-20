c7bb82a6768aba164ef0cbf77819027864eb8de4
stannyu <stanislau.yudzitski@speakap.nl>
2020-10-20 22:14:35 +0200

    Tests for Aged Brie item implemented

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -30,3 +30,36 @@ describe('default item', function () {
     expect(items[0].quality).toBe(0);
   });
 });
+
+describe('Aged Brie', function () {
+  it('should increase quality for Aged Brie by 1', function () {
+    const gildedRose = new Shop([new Item('Aged Brie', 5, 0)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(1);
+  });
+
+  it('should increase quality for Aged Brie by 2 when it expire', function () {
+    const gildedRose = new Shop([new Item('Aged Brie', -1, 41)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(43);
+  });
+
+  it('should not increase quality of Aged Brie over 50', function () {
+    const initialItems = [
+      new Item('Aged Brie', 5, 50),
+      new Item('Aged Brie', 5, 49),
+      new Item('Aged Brie', -5, 48),
+      new Item('Aged Brie', -5, 50),
+    ];
+
+    const gildedRose = new Shop(initialItems);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(50);
+    expect(items[1].quality).toBe(50);
+    expect(items[2].quality).toBe(50);
+    expect(items[3].quality).toBe(50);
+  });
+});
