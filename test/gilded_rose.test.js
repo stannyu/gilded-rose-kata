36addf72d5812fbeaf68f0ac0fe7845323527770
stannyu <stanislau.yudzitski@speakap.nl>
2020-10-20 21:40:06 +0200

    Tests for default item implemented

--- test/gilded_rose.test.js
+++ test/gilded_rose.test.js
@@ -7,3 +7,26 @@ describe('Gilded Rose', function () {
     expect(items[0].name).toBe('foo');
   });
 });
+
+describe('default item', function () {
+  it('should decrease quality for default item by 1', function () {
+    const gildedRose = new Shop([new Item('default', 5, 9)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(8);
+  });
+
+  it('should decrease quality for default item by 2 when sellIn is <= 0', function () {
+    const gildedRose = new Shop([new Item('default', 0, 9)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(7);
+  });
+
+  it('should not decrease quality value when quality is 0', function () {
+    const gildedRose = new Shop([new Item('default', 5, 0)]);
+    const items = gildedRose.updateQuality();
+
+    expect(items[0].quality).toBe(0);
+  });
+});
